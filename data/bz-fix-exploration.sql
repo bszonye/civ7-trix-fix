e6989edf37e929b6d4e1da7de935da3715d9693f
B. Szonye <bszonye@gmail.com>
2025-05-28 03:46:35 -0700

    game version 1.2.1 update

--- data/bz-fix-exploration.sql
+++ data/bz-fix-exploration.sql
@@ -1,18 +1,14 @@
 -- fix Shipbuilding II navy bugs
-UPDATE Modifiers SET SubjectRequirementSetId='MOD_SLTH_BUGFIX_ISNT_NAVY_REQS' WHERE ModifierId='MOD_TECH_OCEAN_TRAVEL_EMBARKED';
+UPDATE Modifiers SET SubjectRequirementSetId='MOD_BZ_DOMAIN_LAND_REQS' WHERE ModifierId='MOD_TECH_OCEAN_TRAVEL_EMBARKED';
 INSERT INTO RequirementSets(RequirementSetId, RequirementSetType) VALUES
-('MOD_SLTH_BUGFIX_ISNT_NAVY_REQS', 'REQUIREMENTSET_TEST_ALL');
+('MOD_BZ_DOMAIN_LAND_REQS', 'REQUIREMENTSET_TEST_ALL');
 INSERT INTO RequirementSetRequirements(RequirementSetId, RequirementId) VALUES
-('MOD_SLTH_BUGFIX_ISNT_NAVY_REQS', 'MOD_SLTH_BUGFIX_ISNT_NAVY_REQS');
-INSERT INTO Requirements(RequirementId, RequirementType, Inverse) VALUES
-('MOD_SLTH_BUGFIX_ISNT_NAVY_REQS', 'REQUIREMENT_UNIT_TAG_MATCHES', 1);
+('MOD_BZ_DOMAIN_LAND_REQS', 'MOD_BZ_DOMAIN_LAND_REQ');
+INSERT INTO Requirements(RequirementId, RequirementType) VALUES
+('MOD_BZ_DOMAIN_LAND_REQ', 'REQUIREMENT_UNIT_DOMAIN_MATCHES');
 INSERT INTO RequirementArguments(RequirementId, Name, Value) VALUES
-('MOD_SLTH_BUGFIX_ISNT_NAVY_REQS', 'Tag', 'UNIT_CLASS_NAVAL');
+('MOD_BZ_DOMAIN_LAND_REQ', 'UnitDomain', 'DOMAIN_LAND');
 
--- fix Ottru not having naval class type (needed for above fix)
--- Also means they get the Hermione model memento bonus
+-- fix Ottru so it has UNIT_CLASS_NAVAL in addition to UNIT_CLASS_OTTRU
+-- (allows it to benefit from the Hermione Class memento)
 INSERT OR REPLACE INTO TypeTags(Type, Tag) VALUES ('UNIT_OTTRU', 'UNIT_CLASS_NAVAL');
-
--- Fix Incense bonus to producing Missionaries
-INSERT OR REPLACE INTO  ModifierArguments(ModifierId, Name, "Value") VALUES
-('MOD_INCENSE_CITY_MISSIONARY_PRODUCTION', 'Percent', '100');
