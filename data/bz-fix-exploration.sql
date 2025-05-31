f44606c1413501edf7ea9b2f707e91630e1a48b5
B. Szonye <bszonye@gmail.com>
2025-05-31 01:59:16 -0700

    revert Basilica + fix Inn type

--- data/bz-fix-exploration.sql
+++ data/bz-fix-exploration.sql
@@ -1,17 +1,16 @@
 -- Shipbuilding mastery: limit modifier to land domain units
-UPDATE Modifiers SET SubjectRequirementSetId='MOD_BZ_DOMAIN_LAND_REQS' WHERE ModifierId='MOD_TECH_OCEAN_TRAVEL_EMBARKED';
-INSERT INTO RequirementSets(RequirementSetId, RequirementSetType) VALUES
-('MOD_BZ_DOMAIN_LAND_REQS', 'REQUIREMENTSET_TEST_ALL');
-INSERT INTO RequirementSetRequirements(RequirementSetId, RequirementId) VALUES
-('MOD_BZ_DOMAIN_LAND_REQS', 'MOD_BZ_DOMAIN_LAND_REQ');
-INSERT INTO Requirements(RequirementId, RequirementType) VALUES
-('MOD_BZ_DOMAIN_LAND_REQ', 'REQUIREMENT_UNIT_DOMAIN_MATCHES');
-INSERT INTO RequirementArguments(RequirementId, Name, Value) VALUES
-('MOD_BZ_DOMAIN_LAND_REQ', 'UnitDomain', 'DOMAIN_LAND');
-
--- Ottru UU: add UNIT_CLASS_NAVAL (in addition to UNIT_CLASS_OTTRU)
--- for compatibility with bonuses like the Hermione Class memento
+UPDATE Modifiers SET SubjectRequirementSetId='MOD_BZ_DOMAIN_LAND_REQS'
+    WHERE ModifierId='MOD_TECH_OCEAN_TRAVEL_EMBARKED';
+INSERT INTO RequirementSets(RequirementSetId, RequirementSetType)
+    VALUES ('MOD_BZ_DOMAIN_LAND_REQS', 'REQUIREMENTSET_TEST_ALL');
+INSERT INTO RequirementSetRequirements(RequirementSetId, RequirementId)
+    VALUES ('MOD_BZ_DOMAIN_LAND_REQS', 'MOD_BZ_DOMAIN_LAND_REQ');
+INSERT INTO Requirements(RequirementId, RequirementType)
+    VALUES ('MOD_BZ_DOMAIN_LAND_REQ', 'REQUIREMENT_UNIT_DOMAIN_MATCHES');
+INSERT INTO RequirementArguments(RequirementId, Name, Value)
+    VALUES ('MOD_BZ_DOMAIN_LAND_REQ', 'UnitDomain', 'DOMAIN_LAND');
+-- Ottru UU: add UNIT_CLASS_NAVAL for compatibility with naval bonuses
 INSERT OR REPLACE INTO TypeTags(Type, Tag) VALUES ('UNIT_OTTRU', 'UNIT_CLASS_NAVAL');
-
--- Guildhall: add DIPLOMACY tag (for Himiko, High Shaman persona)
+-- Himiko, High Shaman: add missing Happiness and Influence types
 INSERT OR REPLACE INTO TypeTags(Type, Tag) VALUES ('BUILDING_GUILDHALL', 'DIPLOMACY');
+INSERT OR REPLACE INTO TypeTags(Type, Tag) VALUES ('BUILDING_TAVERN', 'HAPPINESS');
--- data/bz-fix-modern.sql
+++ data/bz-fix-modern.sql
@@ -1,7 +1,6 @@
--- Bridhadeeswarar Temple and Machu Pickhu: continue adjacency bonuses
+-- Himiko, High Shaman: add missing Happiness and Influence types
+INSERT OR REPLACE INTO TypeTags(Type, Tag) VALUES ('BUILDING_OPERA_HOUSE', 'DIPLOMACY');
+-- Bridhadeeswarar Temple and Machu Pikchu: ageless adjacency bonuses
 UPDATE Adjacency_YieldChanges SET Age = NULL WHERE ID = 'BrihadeeswararTempleWildcardNavigableHappiness';
 UPDATE Adjacency_YieldChanges SET Age = NULL WHERE ID = 'MachuPikchuWildcardCulture';
 UPDATE Adjacency_YieldChanges SET Age = NULL WHERE ID = 'MachuPikchuWildcardGold';
-
--- Opera House: add DIPLOMACY tag (for Himiko, High Shaman persona)
-INSERT OR REPLACE INTO TypeTags(Type, Tag) VALUES ('BUILDING_OPERA_HOUSE', 'DIPLOMACY');
