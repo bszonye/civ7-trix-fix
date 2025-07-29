82b1af2c09840d107483df09a59b963caad196af
B. Szonye <bszonye@gmail.com>
2025-07-29 11:22:12 -0700

    move tweaks & experiments to separate mod

--- README.md
+++ README.md
@@ -1,10 +1,4 @@
 # Trixie's Fixes 1.0.0
 bugfixes and workarounds for vanilla bugs
 
-## if you've stumbled across this mod
-this is my personal collection of essential bug fixes and workarounds
-for civ7, some created by me and some adapted from other mods.  many
-thanks to slothoth, who created or contributed to most of these fixes.
-
-## Slothoth's Assorted Mods
-https://forums.civfanatics.com/threads/slothoths-assorted-mods.698202/
+## changes
--- data/bz-fix-modern.sql
+++ data/bz-fix-modern.sql
@@ -1,6 +1,2 @@
 -- Himiko, High Shaman: add missing Happiness and Influence types
 INSERT OR REPLACE INTO TypeTags(Type, Tag) VALUES ('BUILDING_OPERA_HOUSE', 'DIPLOMACY');
--- Bridhadeeswarar Temple and Machu Pikchu: ageless adjacency bonuses
-UPDATE Adjacency_YieldChanges SET Age = NULL WHERE ID = 'BrihadeeswararTempleWildcardNavigableHappiness';
-UPDATE Adjacency_YieldChanges SET Age = NULL WHERE ID = 'MachuPikchuWildcardCulture';
-UPDATE Adjacency_YieldChanges SET Age = NULL WHERE ID = 'MachuPikchuWildcardGold';
