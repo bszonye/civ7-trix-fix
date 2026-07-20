a866f55189e18b3fd49f8b02bceb4679914495d9
B. Szonye <bszonye@gmail.com>
2026-07-19 17:33:13 -0700

    Trixie's Fixes 1.4.1 (#10)
    * Trixie's Fixes 1.4.1
    
    * fix Theocracy discounts
    
    * limit Theocracy fix to Missionary purchasing
    
    * fix Kanpaku narrative story rewards

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
-# Trixie’s Fixes 1.4.0
-fixes and workarounds for game update 1.4.0
+# Trixie’s Fixes 1.4.1
+fixes and workarounds for game update 1.4.1
 
 ## changes
 gameplay fixes
 - Mikasa and Revenge units: align Tier 3 stats with Battleship
+- Theocracy government: fix Gold towards purchasing Missionaries
+- Toyotomi Hideyoshi narrative stories: fix "Kanpaku" rewards
 
 debugging tools
 - Reload UI hotkey (Ctrl+R)
--- data/bz-fix-modern.sql
+++ data/bz-fix-modern.sql
@@ -1,3 +1,10 @@
 -- align Tier 3 unique units with Battleship stats
 UPDATE Unit_Stats SET Range=3 where UnitType="UNIT_MIKASA_2";
 UPDATE Unit_Stats SET Range=3 where UnitType="UNIT_REVENGE_2";
+-- fix "Kanpaku" narrative story rewards
+UPDATE ModifierArguments SET Value = "ATTRIBUTE_MILITARISTIC"
+WHERE ModifierId = "10168B_NARRATIVE_MODIFIER" AND Name = "AttributeType";
+UPDATE ModifierArguments SET Value = "1"
+WHERE ModifierId = "10168B_NARRATIVE_MODIFIER" AND Name = "Amount";
+UPDATE NarrativeRewardIcons SET RewardIconType = "ATTRIBUTE_MILITARISTIC"
+WHERE NarrativeStoryType = "10168B";
